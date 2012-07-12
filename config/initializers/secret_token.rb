fb08059085dc547b3e170aa133501c6a2ef66cde
vinaysolanki <vinaysolanki2005@gmail.com>
2012-07-12 13:06:10 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gofile::Application.config.secret_token = 'b5338abca29b56e3cc29286fbe6f0acc9b0f0d49251c6d42dde7bc99968a037cd6d3e4d52b19032eadb128da8a626d9273e97eda3bd05b344205b6ae71bdffbf'
